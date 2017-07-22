cedb6252dc9cb400b449ce083620c722fadedc75
alayor <alayor3@gmail.com>
2017-07-21 19:19:20 -0500

    Changing authorization for auth in URLs.

--- prod_start.sh
+++ prod_start.sh
@@ -2,7 +2,7 @@
 java $JAVA_OPTS \
     -Dserver.port=$PORT \
     -Deureka.client.service-url.defaultZone=https://greatapp-discovery-service.herokuapp.com/eureka/ \
-    -Deureka.instance.hostname=authorization-service.greatapp.xyz \
+    -Deureka.instance.hostname=auth.greatapp.xyz \
     -Deureka.instance.prefer-ip-address=false \
     -Dspring.cloud.config.uri=https://greatapp-configuration-service.herokuapp.com/ \
     -jar app.jar \
